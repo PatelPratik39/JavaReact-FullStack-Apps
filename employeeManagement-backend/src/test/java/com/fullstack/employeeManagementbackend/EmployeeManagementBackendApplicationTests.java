c8721b32dea6c71cbba4dae7eb4ad1f0515ee7fe
PatelPratik39 <ptl.pratik717@gmail.com>
2024-05-19 18:41:51 -0500

    Employee Management Project form Spring.io

---
+++ employeeManagement-backend/src/test/java/com/fullstack/employeeManagementbackend/EmployeeManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstack.employeeManagementbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
