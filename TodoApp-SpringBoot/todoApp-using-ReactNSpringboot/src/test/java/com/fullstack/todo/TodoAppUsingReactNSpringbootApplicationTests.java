b70a260576613740231cf398c883e97292a310fd
PatelPratik39 <ptl.pratik717@gmail.com>
2024-05-28 18:15:12 -0500

    cdc

---
+++ TodoApp-SpringBoot/todoApp-using-ReactNSpringboot/src/test/java/com/fullstack/todo/TodoAppUsingReactNSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstack.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppUsingReactNSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
