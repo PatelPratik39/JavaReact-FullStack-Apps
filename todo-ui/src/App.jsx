cc5d071bff3f02b2f8cef215b113f9a5548fba0b
PatelPratik39 <ptl.pratik717@gmail.com>
2024-06-25 17:35:56 -0500

    header and footer component

--- todo-ui/src/App.jsx
+++ todo-ui/src/App.jsx
@@ -1,10 +1,13 @@
-
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 import ListTodoComponent from "./components/ListTodoComponent";
 
 function App() {
   return (
     <>
+      <Header />
       <ListTodoComponent />
+      <Footer />
     </>
   );
 }
---
+++ todo-ui/src/components/Header.jsx
@@ -0,0 +1,20 @@
+
+
+const Header = () => {
+  return (
+    <div>
+        <header>
+            <nav className='navbar nabvbar-expand-md navbar-dark bg-dark'>
+                <div>
+                    <a href='http://localhost:3000' className='navbar-brand'>
+                        Todo Managment App
+                    </a>
+                </div>
+            </nav>
+        </header>
+
+    </div>
+  )
+}
+
+export default Header
