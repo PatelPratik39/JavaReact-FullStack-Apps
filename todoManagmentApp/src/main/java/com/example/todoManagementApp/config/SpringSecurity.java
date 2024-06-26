a733811f74a175c7dd67d5bdc51f375e15f0243a
PatelPratik39 <ptl.pratik717@gmail.com>
2024-06-26 11:25:27 -0500

    Spring security using csrf

---
+++ todoManagmentApp/src/main/java/com/example/todoManagementApp/config/SpringSecurity.java
@@ -0,0 +1,22 @@
+package com.example.todoManagementApp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringSecurity {
+
+    @Bean
+    SecurityFilterChain securityFilterChain( HttpSecurity http ) throws Exception {
+        http.csrf((csrf) -> csrf.disable()).authorizeHttpRequests((authorize) -> {
+            authorize.anyRequest().authenticated();
+        }).httpBasic(Customizer.withDefaults());
+
+
+        return http.build();
+    }
+
+}
