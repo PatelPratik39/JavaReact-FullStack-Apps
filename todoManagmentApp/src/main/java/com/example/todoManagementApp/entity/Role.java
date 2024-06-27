48a474ee81749e3ed70a10a442e20e81ef50ceeb
PatelPratik39 <ptl.pratik717@gmail.com>
2024-06-26 22:31:24 -0500

    Created join columns using user id and role id for Many to many relationship between user and roles

---
+++ todoManagmentApp/src/main/java/com/example/todoManagementApp/entity/Role.java
@@ -0,0 +1,19 @@
+package com.example.todoManagementApp.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+}
